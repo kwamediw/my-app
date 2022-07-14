03682c96f4a6313138c67540636cfdd57cfc1e19
kwamediw <kwamediwanix@gmail.com>
2022-07-15 01:31:44 +0200

    add-styles

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Profile from './Components/Profile';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<h1>Moja</h1>
+			<Profile />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Profile() {
+	return (
+		<div className='profile-items'>
+			<h1>Place Live</h1>
+			<h2>Car Insurance</h2>
+		</div>
+	);
+}
